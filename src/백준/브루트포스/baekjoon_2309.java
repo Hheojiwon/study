2d3d2e76c65c6cd9cbd8597d8468ea0c28b68670
Hheojiwon <0618amy@naver.com>
2025-01-20 03:08:48 +0900

    백준 2309

--- src/백준/브루트포스/baekjoon_2309.java
+++ src/백준/브루트포스/baekjoon_2309.java
@@ -1,2 +1,46 @@
-package 백준.브루트포스;public class baekjoon_2309 {
+package 백준.브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon_2309 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp= arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length ; j++) {
+                if (sum - arr[i] - arr[j]==100) {
+                    num1 = i;
+                    num2 = j;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i != num1 && i != num2) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
 }
