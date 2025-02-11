ffa9e11cc951422cc989f5ed6544d1a97603a2ff
Hheojiwon <0618amy@naver.com>
2025-02-12 02:44:34 +0900

    추 가

--- src/백준/큐/programmers_기능개발.java
+++ src/백준/큐/programmers_기능개발.java
@@ -1,2 +1,46 @@
-package 백준.큐;public class programmers_기능개발 {
+package 백준.큐;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class programmers_기능개발 {
+
+    public static void main(String[] args) {
+        int[] progresses = {90,30,55};
+        int[] speeds = {2,1};
+        solution(progresses, speeds);
+    }
+    public static int[] solution(int[] progresses, int[] speeds) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            if ((100 - progresses[i]) % speeds[i] == 0) {
+                deque.offer((100 - progresses[i]) / speeds[i]);
+            } else {
+                deque.offer(((100 - progresses[i]) / speeds[i]) + 1);
+            }
+        }
+
+        int day = deque.poll();
+        int count = 1;
+
+        while (!deque.isEmpty()) {
+            if (day >= deque.getFirst()) {
+                count++;
+                deque.poll();
+            } else {
+                list.add(count);
+                count = 1;
+                day = deque.poll();
+            }
+        }
+        list.add(count);
+
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+
 }
