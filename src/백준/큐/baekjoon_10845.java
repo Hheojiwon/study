5567f18beac0d5ae8fbc3e1a280cb70888853805
Hheojiwon <0618amy@naver.com>
2025-02-22 03:06:20 +0900

    백준 10845

--- src/백준/큐/baekjoon_10845.java
+++ src/백준/큐/baekjoon_10845.java
@@ -1,2 +1,41 @@
-package 백준.큐;public class baekjoon_10845 {
+package 백준.큐;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class baekjoon_10845 {
+    public static void main(String[] args) throws IOException {
+
+        Queue<Integer> q = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int backNumber = 0;
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            if (input.contains("push")) {
+                String[] strings = input.split(" ");
+                backNumber = Integer.parseInt(strings[1]);
+                q.offer(backNumber);
+            }
+            if (input.equals("pop")) {
+                System.out.println(q.isEmpty() ? "-1" : q.poll());
+            }
+            if (input.equals("size")) {
+                System.out.println(q.size());
+            }
+            if (input.equals("empty")) {
+                System.out.println(q.isEmpty() ? 1 : 0);
+            }
+            if (input.equals("front")) {
+                System.out.println(q.isEmpty() ? "-1" : q.peek());
+            }
+            if (input.equals("back")) {
+                System.out.println(q.isEmpty() ? "-1" : backNumber);
+            }
+        }
+    }
 }
