9a509e3806071278c1ccdad784c8ec68c9159d49
Hheojiwon <0618amy@naver.com>
2025-02-24 03:07:24 +0900

    백준 11399

--- src/백준/greedy/baekjoon_11399.java
+++ src/백준/greedy/baekjoon_11399.java
@@ -1,2 +1,31 @@
-package 백준.greedy;public class baekjoon_11399 {
+package 백준.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class baekjoon_11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] str = br.readLine().split(" ");
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+
+        Arrays.sort(arr);
+
+        int prev = 0;
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += prev + arr[i];
+            prev += arr[i];
+        }
+
+        System.out.println(sum);
+    }
+
 }
