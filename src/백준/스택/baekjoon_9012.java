0ef3dbd283daa651810a2cdca4ac3ab087d5a053
Hheojiwon <0618amy@naver.com>
2025-02-28 03:10:41 +0900

    백준 9012

--- src/백준/스택/baekjoon_9012.java
+++ src/백준/스택/baekjoon_9012.java
@@ -1,2 +1,21 @@
-package 백준.스택;public class baekjoon_9012 {
+package 백준.스택;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon_9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            while (input.contains("()")) {
+                input= input.replace("()", "");
+            }
+
+            System.out.println(input.length() == 0 ? "YES" : "NO");
+        }
+    }
 }
