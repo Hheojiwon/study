425635c2c5b6ae31599569b41e0d9f2aa4e38749
Hheojiwon <0618amy@naver.com>
2025-03-04 03:02:03 +0900

    백준 1541

--- src/백준/greedy/baekjoon_1541.java
+++ src/백준/greedy/baekjoon_1541.java
@@ -1,8 +1,28 @@
 package 백준.greedy;
 
-public class baekjoon_1542 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-
+public class baekjoon_1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str= br.readLine().split("-");
+        List<Integer> list= new ArrayList<>();
+        for (String s : str){
+            int sum = 0;
+            String[] split= s.split("\\+");
+            for (String s1 : split){
+                sum+= Integer.parseInt(s1);
+            }
+            list.add(sum);
+        }
+        long answer = list.get(0);
+        for (int i=1; i<list.size(); i++){
+            answer-= list.get(i);
+        }
+        System.out.println(answer);
     }
 }
