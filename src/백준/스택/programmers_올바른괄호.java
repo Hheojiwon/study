e80b888e28e4500784b07904edb2ff4725bc15d3
Hheojiwon <0618amy@naver.com>
2025-03-14 02:54:44 +0900

    프로그래머스 올바른 괄호

--- src/백준/스택/programmers_올바른괄호.java
+++ src/백준/스택/programmers_올바른괄호.java
@@ -1,2 +1,32 @@
-package 백준.스택;public class programmers_올바른괄호 {
+package 백준.스택;
+
+import java.util.Stack;
+
+public class programmers_올바른괄호 {
+    public static void main(String[] args) {
+        String s = "(()(";
+        System.out.println(solution(s));
+    }
+
+    public static boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (!stack.isEmpty()) {
+                char before = stack.peek();
+                if (before == '(' && s.charAt(i) == ')') {
+                    stack.pop();
+                } else {
+                    stack.push(s.charAt(i));
+                }
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        if (!stack.isEmpty()) answer = false;
+
+        return answer;
+    }
 }
