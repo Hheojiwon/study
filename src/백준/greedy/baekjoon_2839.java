c0885c0cc93cfacf73ae1204cbed539c0e223b29
Hheojiwon <0618amy@naver.com>
2025-03-15 02:56:28 +0900

    백준_ 2839

--- src/백준/greedy/baekjoon_2839.java
+++ src/백준/greedy/baekjoon_2839.java
@@ -1,2 +1,26 @@
-package 백준.greedy;public class baekjoon_2839 {
+package 백준.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon_2839 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        while (N >= 0) {
+            if (N % 5 == 0) {
+                count += N / 5;
+                N = 0;
+                break;
+            }
+            N -= 3;
+            count++;
+        }
+        if (N != 0) {
+            count = -1;
+        }
+        System.out.println(count);
+    }
 }
