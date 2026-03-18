20b52d0edfa62bd89c89f1dc4f373377004074da
Hheojiwon <0618amy@naver.com>
2026-03-18 11:14:48 +0900

    백준 9095

---
+++ src/백준/DP/baekjoon_9095.java
@@ -0,0 +1,26 @@
+package 백준.DP;
+import java.io.*;
+import java.util.*;
+
+public class baekjoon_9095 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int T= Integer.parseInt(br.readLine());
+
+        int[] dp = new int [12];
+        dp[1]= 1;
+        dp[2]= 2;
+        dp[3]= 4;
+
+        for (int i=4; i<12; i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+
+        }
+
+        for (int j=0; j<T; j++){
+            int N = Integer.parseInt(br.readLine());
+            System.out.println(dp[N]);
+        }
+
+    }
+}
